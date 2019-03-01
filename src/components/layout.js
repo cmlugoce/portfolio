094a63e1668f8d0c0acb5e261f7b8c6d1885b0e8
Cristina <cmlugoce@syr.edu>
2019-03-01 17:04:43 -0500

    add styling & header

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,15 +20,15 @@ const Layout = ({ children }) => (
       
         
         <div
-          style={{
-            //margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 0rem 0rem`,
-            paddingTop: 0,
-          }}
+          // style={{
+          //   //margin: `0 auto`,
+          //   maxWidth: 960,
+          //   padding: `0px 0rem 0rem`,
+          //   paddingTop: 0,
+          // }}
         >
         
-        <NavBar />
+        <Header />
         
           <main>
          
