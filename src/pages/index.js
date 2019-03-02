151813c835e7173c8802a2481a4b436abf201a64
Cristina Lugo <cmlugoce@syr.edu>
2019-03-02 14:59:53 -0500

    add particle js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,37 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import Skills from '../components/skills'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ParticlesBG from '../components/particlejs'
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  background: transparent;
+  overflow: hidden;
+ margin-top: -2%;
+  position: relative;
+  z-index: 4;
+  
+ 
+  
+  }
+  
+  
+`;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Cristina Lugo" keywords={[`gatsby`, `application`, `react`]} />
+    <Wrapper>
+    <ParticlesBG />
     <div className='bg'>
-    
-    <h1 id='hello' style ={{color: `white`, fontSize: `6em`, fontWeight: `bold`,  marginLeft: `10%`}}>Hello<span style={{color: `#fd5e53`}}>.</span></h1>
+   
+     
+    <h1 id='hello' style ={{  fontWeight: `bold`,  marginLeft: `6%`, zIndex: 1}}>Hola!</h1>
     <h2 id='cap' style={{marginLeft: `10%`}}> I’m Cristina, a web developer, geologist and nature lover</h2>
     
     </div>
-    
+    <ParticlesBG />
+    </Wrapper>
     <Me />
    
     
