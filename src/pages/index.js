aecc00b6e3716dca40b3214f39620e635d83d900
Cristina <cmlugoce@syr.edu>
2019-03-04 11:52:56 -0500

    revert to original layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Me from '../components/me'
 import Layout from "../components/layout"
 
 import SEO from "../components/seo"
-import universe from '../images/universe.jpg'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ParticlesBG from '../components/particlejs'
 import styled from 'styled-components';
@@ -19,18 +19,15 @@ const Wrapper = styled.div`
   @media (max-width: 380px){
     margin-top: -7%;
   }
-
   @media (max-width: 450px){
     margin-top: -5.6%;
   }
-
   @media (max-width: 900px){
     margin-top: -1.5em;
   }
  @media(max-width: 1280px){
    margin-top: -1.5em;
  }
-
  
   }
   
@@ -48,7 +45,7 @@ const IndexPage = () => (
     <div className='bg'>
     
     
-    
+   
     <h1 id='hello' style ={{  fontWeight: `bold`,  marginLeft: `6%`, zIndex: 1}}>Hola!</h1>
     <h2 id='cap' style={{marginLeft: `10%`}}> I’m Cristina, a web developer, geologist and nature lover</h2>
    
@@ -56,7 +53,6 @@ const IndexPage = () => (
     <ParticlesBG />
     </Wrapper>
     <>
-    
     <Me />
    
     </>
