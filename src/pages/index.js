f178b37f89ab199846a103a4268670b93f4b3f2c
Cristina <cmlugoce@syr.edu>
2019-03-08 11:34:40 -0500

    add styled components and image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import { Link } from "gatsby"
 import "../components/layout.css"
 import Me from '../components/me'
 import Layout from "../components/layout"
-
+import Image from '../components/image'
 import SEO from "../components/seo"
-
+import universe from '../images/universe.jpg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ParticlesBG from '../components/particlejs'
 import styled from 'styled-components';
@@ -15,6 +15,7 @@ const Wrapper = styled.div`
   overflow: hidden;
   margin-top: -1.69%;
   position: relative;
+  z-index: 1;
  
   @media (max-width: 380px){
     margin-top: -7%;
@@ -34,6 +35,44 @@ const Wrapper = styled.div`
   
 `;
 
+const BgImage = styled(Image)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: -1;
+  height: 100vh; // or whatever
+  // Adjust image positioning (if image covers area with defined height) and add font-family for polyfill
+  & > img {
+    object-fit: cover !important; // or whatever
+    object-position: 0% 0% !important; // or whatever
+    font-family: 'object-fit: cover !important; object-position: 0% 0% !important;' // needed for IE9+ polyfill
+  }
+
+  
+`
+
+const Cap = styled.h1`
+ 
+ background: transparent;
+ z-index: 1;
+ position: absolute;
+  top: 20%;
+  left: 20%;
+  transform: translate(-50%,-50%);
+
+
+`
+
+const Info = styled.h2`
+z-index: 1;
+ position: absolute;
+  top: 35%;
+  left: 10%;
+  
+
+
+`
 const IndexPage = () => (
   <Layout>
     <SEO title="Cristina Lugo" keywords={[`gatsby`, `application`, `react`]} 
@@ -42,17 +81,24 @@ const IndexPage = () => (
     
     <Wrapper>
     <ParticlesBG  />
-    <div className='bg'>
+    <Cap id='hello'>Hola!</Cap>
+    <Info id='cap'>I’m Cristina, a web developer, 
+      geologist and nature lover</Info>
+    <BgImage />
+
     
+    {/* <div className='bg'>  
     
    
-    <h1 id='hello' style ={{  fontWeight: `bold`,  marginLeft: `6%`, zIndex: 1}}>Hola!</h1>
+    <h1 id='hello' style ={{  fontWeight: `bold`,  marginLeft: `6%`}}>Hola!</h1>
+     
     <h2 id='cap' style={{marginLeft: `10%`}}> I’m Cristina, a web developer, geologist and nature lover</h2>
-   
-    </div>
+    
+    </div> */}
     <ParticlesBG />
     </Wrapper>
     <>
+    
     <Me />
    
     </>
