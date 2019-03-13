4beef96c3a5385fc64fc37fb52ca0c634821e77e
Cristina <cmlugoce@syr.edu>
2019-03-13 17:09:14 -0400

    try to remove duplicate fav

--- src/components/layout.js
+++ src/components/layout.js
@@ -37,7 +37,7 @@ const Layout = ({ children }) => (
         },
       ]}
       link={[
-        { rel: 'icon', type: 'image/png', href: `${icon}` }
+        { rel: 'favicon', type: 'image/png', href: `${icon}` }
     ]}
     />
         
